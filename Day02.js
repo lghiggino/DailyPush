40414a25735aca95148784937b403d90eef71bd3
lghiggino <lghiggino@gmail.com>
2021-12-08 09:54:12 -0300

    day 02 is done

--- Day02.js
+++ Day02.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 
-fs.readFile('./day02dataset.txt', 'utf8', (err,data) => {
-    if(err){
+fs.readFile('./day02dataset.txt', 'utf8', (err, data) => {
+    if (err) {
         console.log(err.message)
         return
     }
@@ -13,21 +13,28 @@ fs.readFile('./day02dataset.txt', 'utf8', (err,data) => {
         up: 0
     }
     arrayOfData.forEach(element => {
-        if (element.includes("forward")){
+        if (element.includes("forward")) {
             const num = element.split(" ")[1]
             object.forward += +num
         }
-        if (element.includes("down")){
+        if (element.includes("down")) {
             const num = element.split(" ")[1]
             object.down += +num
         }
-        if (element.includes("up")){
+        if (element.includes("up")) {
             const num = element.split(" ")[1]
             object.up += +num
         }
     })
-    console.log(object)
+    // console.log(object)
     const calc = (object.down - object.up) * object.forward
     console.log(calc)
 })
 
+//no idea here
+fs.readFile('./day02dataset.txt', 'utf-8', (err, data) => {
+    const value = data.split(/\n(?!$)/)
+    .map(x => x.split` `)
+    .reduce((a, [d, n]) => (({ f() { a[0] = +n + a[0]; a[1] = a[1] + n * a[2] }, d() { a[2] = +n + a[2] }, u() { a[2] = -n + a[2] } })[d[0]](), a), [0, 0, 0])
+    console.log(value)
+})
