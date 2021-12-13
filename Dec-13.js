5e55076be02e920f57513c3f4def1607480282eb
lghiggino <lghiggino@gmail.com>
2021-12-13 20:47:42 -0300

    regex starts and ends with same vowel

--- Dec-13.js
+++ Dec-13.js
@@ -16,4 +16,16 @@ function vowelsAndConsonants(s) {
     }
 }
 
-https://www.hackerrank.com/challenges/js10-loops/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+//Complete the function in the editor below by returning a RegExp object, , that matches any string  that begins and ends with the same vowel. Recall that the English vowels are a, e, i, o, and u.
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    let re = /^[a].*[a]$|^[e].*[e]$|^[i].*[i]$|^[o].*[o]$|^[u].*[u]$/g
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
