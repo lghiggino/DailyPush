52f0f94ae382fe5613744558fa900b085c88ded9
lghiggino <lghiggino@gmail.com>
2021-12-17 18:50:45 -0300

    Dec-16 passando em todos os testes com array.sort + new Set(nums)

--- Dec-16.js
+++ Dec-16.js
@@ -2,16 +2,25 @@ function getSecondLargest(nums) {
     if (nums.length <= 2) {
         return undefined
     }
-    let sorted = nums.sort()
-
-    for (let i = nums.length - 2; i > 0; i--) {
-        if (sorted[i] != sorted[i + 1]) {
-            return sorted[i]
+    let sorted = nums.sort(function (a, b) {
+        if (a > b) {
+          return 1;
         }
-    }
+        if (a < b) {
+          return -1;
+        }
+        return 0;
+      })
+    const uniqueNums = [...new Set(nums)]
+    const index = uniqueNums.length-2
+    return uniqueNums[index]
 }
 
 //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+
+getSecondLargest([2, 3, 6, 6, 5])
+getSecondLargest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
 
 
 module.exports = { getSecondLargest }
