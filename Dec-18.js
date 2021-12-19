18c67c21e9a2d471f2c2a1af5b7a547a979b3f95
lghiggino <lghiggino@gmail.com>
2021-12-19 18:31:20 -0300

    refatorando o teste

--- Dec-18.js
+++ Dec-18.js
@@ -1,5 +1,14 @@
+// function groupByKey(array, key) {
+//   // if (!array || array.length === 0) { return }
+//   return array
+//     .reduce((hash, obj) => {
+//       if (obj[key] === undefined) return hash;
+//       return Object.assign(hash, { [obj[key]]: (hash[obj[key]] || []).concat(obj) })
+//     }, [])
+// }
+
 function groupByKey(array, key) {
-  // if (!array || array.length === 0) { return }
+  if (!array || array.length === 0) { return }
   return array
     .reduce((hash, obj) => {
       if (obj[key] === undefined) return hash;
