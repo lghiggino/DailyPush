18cfabf77c6b15bb845e86aeea821567a3f16e74
lghiggino <lghiggino@gmail.com>
2021-12-23 09:14:15 -0300

    expanded tests

--- test/dec23.test.js
+++ test/dec23.test.js
@@ -8,6 +8,30 @@ const testArray0 = [
     { x: 3, y: 4 },
     { x: 4, y: 5 }
 ]
+
+const testArray8 = [
+    { x: 1, y: 1 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+    { x: 3, y: 4 },
+    { x: 4, y: 5 },
+    { x: 1, y: 1 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+    { x: 3, y: 4 },
+    { x: 4, y: 5 },
+    { x: 1, y: 1 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+    { x: 3, y: 4 },
+    { x: 4, y: 5 },
+    { x: 1, y: 1 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+    { x: 3, y: 4 },
+    { x: 4, y: 5 }
+]
+
 const testArray2 = [
     { x: 1, y: 1 },
     { x: 2, y: 3 },
@@ -19,6 +43,12 @@ const testArray2 = [
 
 
 describe("Tests getCount in multiple objects", () => {
+    it("should return 0", () => {
+        expect(getCount(testArray0)).toBe(0)
+    })
+    it("should return 8", () => {
+        expect(getCount(testArray8)).toBe(8)
+    })
     it("should return 2", () => {
         expect(getCount(testArray2)).toBe(2)
     })
