c983f1ce64086fb119ff30e6f5c1bbe5cea2bcdb
lghiggino <lghiggino@gmail.com>
2021-12-25 07:57:59 -0300

    Queue Data Structure and test

--- Dec-22-Queue.js
+++ Dec-22-Queue.js
@@ -1,3 +1,48 @@
+/*Queue - FIFO
+we build the queue by adding items to the end of it.
+we deconstruct the queue by removing items from the start of it
+
+commom methods are:
+  enqueue() -- adding to the queue
+  dequeue() -- removing from the queu
+*/
+
+//https://www.youtube.com/watch?v=1AJ4ldcH2t4
+
+
+class Queue {
+    constructor() {
+        this.storage = {}
+        this.head = 0
+        this.tail = 0
+    }
+
+    enqueue(element) {
+        this.storage[this.tail] = element
+        this.tail++
+    }
+
+    dequeue() {
+        let removed = this.storage[this.head]
+        delete this.storage[this.head]
+        this.head++
+        return removed
+    }
+
+    peek(){
+        let head = this.storage[this.head]
+        return head
+    }
+
+}
+
+module.exports = { Queue }
+
+
+
+
+
+
 function modifyArray(nums) {
     const mapped = nums.map((item) => {
         if (item % 2 === 0) {
@@ -6,10 +51,9 @@ function modifyArray(nums) {
             return item * 3
         }
     })
-    console.log("log",mapped)
+    console.log("log", mapped)
     return mapped
 
 }
 
-
-modifyArray([1, 2, 3, 4, 5])
+// modifyArray([1, 2, 3, 4, 5])
---
+++ test/dec22.test.js
@@ -0,0 +1,21 @@
+import { Queue } from "../Dec-22-Queue";
+
+describe("checks if the Queue methods produce the expected results", () => {
+    const animalQueue = new Queue()
+    animalQueue.enqueue("abelha")
+    animalQueue.enqueue("borboleta")
+    animalQueue.enqueue("coelho")
+
+    console.log(animalQueue)
+
+    it("enqueue() should add a new animal to the start of the queue", () => {
+        animalQueue.enqueue("dromedário")
+        console.log(animalQueue.peek())
+        expect(animalQueue.peek()).toBe("abelha")
+    })
+    it("dequeue() should add element in the Queue", () => {
+        let animal = animalQueue.dequeue()
+        expect(animal).toBe("abelha")
+    })
+
+})
