d76108977b36bc92813002396bbcd6e6071caa8a
lghiggino <lghiggino@gmail.com>
2022-01-01 19:54:20 -0300

    staircase dailyPush

---
+++ test/2022/January/jan01.test.ts
@@ -0,0 +1,10 @@
+import { staircase } from "../../../src/2022/January/jan01";
+
+describe("staircase", () => {
+    it("should return '\n #\n##' for 2", () => {
+        expect(staircase(2)).toBe("\n #\n##")
+    })
+    it("should return '      #\n    ##\n   ###\n  ####\n #####\n######' for 6", () => {
+        expect(staircase(6)).toBe('\n     #\n    ##\n   ###\n  ####\n #####\n######')
+    })
+})
