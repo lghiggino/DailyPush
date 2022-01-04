1b451a7375e2e863d636b307d1a448f162c0ce27
lghiggino <lghiggino@gmail.com>
2022-01-04 10:04:37 -0300

    daily push

---
+++ test/2022/January/jan03.test.ts
@@ -0,0 +1,10 @@
+import {birthdayCakeCandles} from "../../../src/2022/January/jan03"
+
+describe("birthdayCakeCandles", () => {
+    it("should return 2 for [1, 2, 3, 4, 5]", () => {
+        expect(birthdayCakeCandles([1, 2, 3, 4, 5, 5])).toBe(2)
+    })
+    it("should return 5 for [1, 1, 1, 1, 1]", () => {
+        expect(birthdayCakeCandles([1, 1, 1, 1, 1])).toBe(5)
+    })
+})
