0e076ce27b5d79bf8e761c393241513d129010be
lghiggino <lghiggino@gmail.com>
2022-01-14 10:07:28 -0300

    test and function first commit

---
+++ src/2022/January/jan12.ts
@@ -0,0 +1,48 @@
+/**
+ Two friends Anna and Brian, are deciding how to split the bill at a dinner. Each will only pay for the items they consume. Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
+
+For example, assume the bill has the following prices: . Anna declines to eat item  which costs . If Brian calculates the bill correctly, Anna will pay . If he includes the cost of , he will calculate . In the second case, he should refund  to Anna.
+
+Function Description
+
+Complete the bonAppetit function in the editor below. It should print Bon Appetit if the bill is fairly split. Otherwise, it should print the integer amount of money that Brian owes Anna.
+
+bonAppetit has the following parameter(s):
+
+bill: an array of integers representing the cost of each item ordered
+k: an integer representing the zero-based index of the item Anna doesn't eat
+b: the amount of money that Anna contributed to the bill
+
+
+Output Format
+
+If Brian did not overcharge Anna, print Bon Appetit on a new line; otherwise, print the difference (i.e., ) that Brian must refund to Anna. This will always be an integer.
+
+Sample Input 
+
+4 1
+3 10 2 9
+12
+Sample Output 
+
+5
+
+
+
+
+Sample Input 1
+
+4 1
+3 10 2 9
+7
+Sample Output 1
+
+Bon Appetit
+Explanation 1
+Anna didn't eat item , but she shared the rest of the items with Brian. The total cost of the shared items is  and, split in half, the cost per person is . Because , we print Bon Appetit on a new line.
+ */
+
+export function bonAppetit(bill: number[], k: number, b: number): void {
+    // Write your code here
+
+}
---
+++ test/2022/January/jan12.test.ts
@@ -0,0 +1,11 @@
+import { bonAppetit } from "../../../src/2022/January/jan12";
+
+describe("bonAppetit", () => {
+    it("should return 5 for...", () => {
+        expect(bonAppetit([3, 10, 2, 9], 1, 12)).toBe(5)
+    })
+
+    it("should return 'Bon Appetit for...", () => {
+        expect(bonAppetit([3, 10, 2, 9], 1, 7)).toBe('Bon Appetit')
+    })
+})
