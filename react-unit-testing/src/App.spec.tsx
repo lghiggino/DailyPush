6629abb28e8deeaed5e63f676f30ccd6f996c7d6
lghiggino <lghiggino@gmail.com>
2022-05-01 22:13:53 -0300

    test the changed state from useState

--- react-unit-testing/src/App.spec.tsx
+++ react-unit-testing/src/App.spec.tsx
@@ -1,17 +1,24 @@
 import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
-test('sum', () => {
-    function sum(a: number, b: number): number {
-        return a + b
-    }
+describe('APP', () => {
+    it('should render list items', () => {
+        const { getByText } = render(<App />)
 
-    expect(sum(1, 1)).toBe(2)
+        expect(getByText('Diego')).toBeInTheDocument()
+        expect(getByText('Rodz')).toBeInTheDocument()
+        expect(getByText('Mayk')).toBeInTheDocument()
+    })
+
+    it('should be able to add new item to list', async () => {
+        const { getByText } = render(<App />)
+        const addButton = getByText('Adicionar')
+
+        await userEvent.click(addButton);
+
+        expect(getByText('Novo')).toBeInTheDocument()
+    })
 })
 
-
-test('app', () => {
-    const { getByText } = render(<App />)
-
-    expect(getByText('Hello World')).toBeTruthy();
-})
+// https://youtu.be/edXudaVB0Bg?t=2680
