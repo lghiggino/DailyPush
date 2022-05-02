4ec6e0e7007d2624f8d998f7cc2b907b391eaa96
lghiggino <lghiggino@gmail.com>
2022-05-02 09:09:11 -0300

    experimental vue project first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+
+import { ref } from "vue";
+
+defineProps<{
+  contextMessage: string;
+}>();
+
+const contextMessage = ref('banana');
+</script>
+
+
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" contextMessage="banana" />
+</template>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
