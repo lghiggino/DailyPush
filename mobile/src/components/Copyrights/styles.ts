4a94c908e5c077c2b82f099e9ebfa2bc77eb38eb
lghiggino <lghiggino@gmail.com>
2022-05-06 08:05:10 -0300

    Options component + gestureHandlerRootHOC(Widget)

---
+++ mobile/src/components/Copyrights/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../theme';
+
+export const styles = StyleSheet.create({
+  container: {},
+  text: {
+    color: theme.colors.text_secondary,
+    fontSize: 12,
+    fontFamily: theme.fonts.medium
+  },
+});
---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+
+import { styles } from './styles';
+import { ChatTeardropDots } from 'phosphor-react-native';
+import { theme } from '../../theme';
+import { Options } from '../Options';
+
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand()
+  }
+
+  return (
+    <>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleOpen}
+      >
+        <ChatTeardropDots
+          size={24}
+          weight={'bold'}
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Options />
+      </BottomSheet>
+    </>
+  );
+}
+
+export default gestureHandlerRootHOC(Widget);
