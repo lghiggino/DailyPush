848acea264508996a3f834c5aaf4c1830799a583
lghiggino <lghiggino@gmail.com>
2022-05-13 08:01:53 -0300

    svelte first try

---
+++ svelte/src/lib/Counter.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  let count: number = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<div class="banana">
+  <button on:click={increment}>
+    Clicks: {count}
+  </button>
+
+</div>
+
+<style>
+  button {
+    font-family: inherit;
+    font-size: inherit;
+    padding: 1em 2em;
+    color: #ff3e00;
+    background-color: rgba(255, 62, 0, 0.1);
+    border-radius: 2em;
+    border: 2px solid rgba(255, 62, 0, 0);
+    outline: none;
+    width: 200px;
+    font-variant-numeric: tabular-nums;
+    cursor: pointer;
+  }
+
+  .banana {
+    max-width: 25%;
+    margin: 0 auto;
+  }
+
+  button:focus {
+    border: 2px solid #ff3e00;
+  }
+
+  button:active {
+    background-color: rgba(255, 62, 0, 0.2);
+  }
+</style>
