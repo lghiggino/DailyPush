b308242242602406ede3e370c350c730e9d5a1ac
lghiggino <lghiggino@gmail.com>
2022-05-14 09:59:04 -0300

    re-routing

--- BackEnd/prismaServer/src/routes/index.ts
+++ BackEnd/prismaServer/src/routes/index.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
+import { prisma } from '../prisma'
 
 export const routes = Router()
 
 routes.get('/', (req, res) => {
     res.send(`<div> 
-    <a href="https://feedback-widget-gules.vercel.app/"> 
-    <h1>NLW Return</h1>   
-    </a>
+    <h1>Hello</h1>   
     </div>`)
 })
 
@@ -16,50 +15,24 @@ routes.get('/health', (req, res) => {
     res.json(`I am healthy at ${Date.now()}`)
 })
 
-// routes.post('/feedbacks', async (req, res) => {
-//     const { type, comment, screenshot } = req.body
+routes.get('/courses', async (req, res) => {
+    try {
+        const courses = await prisma.courses.findMany()
+        res.json(courses)
+    } catch (error) {
+        console.error(error)
+    }
+})
 
-//     const prismaFeedbackRepository = new PrismaFeedbacksRepository()
-//     const nodemailerMailAdapter = new NodemailerMailAdapter()
-//     const submitFeedbackService = new SubmitFeedbackService(
-//         prismaFeedbackRepository,
-//         nodemailerMailAdapter
-//     )
+routes.get('/courses/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        console.log("COURSE ID:>>", id)
+        const oneCourse = await prisma.courses.findMany({
+            where: { id }
+        })
+    } catch (error) {
+        console.error(error)
+    }
+})
 
-//     const createdAt = dayjs().format('yyyy-mm-dd')
-
-//     const feedback = await submitFeedbackService.execute({
-//         type,
-//         comment,
-//         screenshot,
-//         createdAt
-//     })
-
-//     return res.status(201).json({ data: feedback })
-// })
-
-// routes.post('/team', async (req, res) => {
-//     const { name } = req.body
-
-//     const teamMemberRepository = new PrismaTeamMemberRepository()
-//     const teamMemberService = new TeamMemberService(teamMemberRepository)
-
-//     const createdAt = dayjs().format('yyyy-mm-dd')
-
-//     const teamMember = await teamMemberService.execute({
-//         name,
-//         createdAt
-//     })
-
-//     return res.status(201).json({ data: teamMember })
-// })
-
-// routes.get('/team', async (req, res) => {
-//     const teamMemberRepository = new PrismaTeamMemberRepository()
-//     const teamMemberService = new TeamMemberService(teamMemberRepository)
-
-
-//     const allTeamMembers = await teamMemberService.getAll()
-
-//     return res.status(201).json({ data: allTeamMembers })
-// })
