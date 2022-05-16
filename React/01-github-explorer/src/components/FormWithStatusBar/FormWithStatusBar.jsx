6ddc37eb01761b879a3acccf10914011f89d9d84
lghiggino <lghiggino@gmail.com>
2022-05-16 06:30:03 -0300

    working progressBar and Form

--- React/01-github-explorer/src/components/FormWithStatusBar/FormWithStatusBar.jsx
+++ React/01-github-explorer/src/components/FormWithStatusBar/FormWithStatusBar.jsx
@@ -1,27 +1,91 @@
-import { useState } from "react";
-import './FormWithStatusBar.scss';
+import { useState, useEffect } from "react";
+import "./FormWithStatusBar.scss";
 import { StatusBar } from "./StatusBar";
 
 export function FormWithStatusBar() {
     const [formData, setFormData] = useState({
-        type: '',
-        name: '',
-        state: '',
-        city: ''
+        type: "",
+        name: "",
+        state: "",
+        city: ""
     })
 
     const [progress, setProgress] = useState(0)
 
-    function handleProgress() { 
-        console.log("formData>>>", formData)
-        if(formData.city.length){
-            setProgress(p => p + 25)
+    const [objectProgress, setObjectProgress] = useState({
+        type: "unchecked",
+        name: "unchecked",
+        state: "unchecked",
+        city: "unchecked"
+    })
+
+    function handleValidationAndProgress() {
+        if (formData.type) {
+            setObjectProgress(data => ({ ...data, type: "checked" }))
+        }
+        if (formData.name.length) {
+            setObjectProgress(data => ({ ...data, name: "checked" }))
+        }
+        if (formData.state) {
+            setObjectProgress(data => ({ ...data, state: "checked" }))
+        }
+        if (formData.city.length) {
+            setObjectProgress(data => ({ ...data, city: "checked" }))
         }
     }
 
-    useState(() => {
-        handleProgress()
+
+    function handleName(ev) {
+        if (formData.name.length > 0 && objectProgress.name === "checked") {
+            return
+        } else {
+            setFormData(data => ({ ...data, name: ev.target.value }))
+            handleValidationAndProgress()
+        }
+
+    }
+
+    function handleType(ev) {
+        if (formData.type.length > 0 && objectProgress.type === "checked") {
+            return
+        } else {
+            setFormData(data => ({ ...data, type: ev.target.value }))
+            handleValidationAndProgress()
+        }
+    }
+
+    function handleState(ev) {
+        if (formData.state.length > 0 && objectProgress.state === "checked") {
+            return
+        } else {
+            setFormData(data => ({ ...data, state: ev.target.value }))
+            handleValidationAndProgress()
+        }
+    }
+
+    function handleCity(ev) {
+        if (formData.city.length > 0 && objectProgress.city === "checked") {
+            return
+        } else {
+            setFormData(data => ({ ...data, city: ev.target.value }))
+            handleValidationAndProgress()
+        }
+    }
+
+    function handleProgressBarCompletion() {
+        const checkCountArr = Object.entries(objectProgress).filter((item) => {
+            return item[1] === 'checked'
+        })
+        setProgress(checkCountArr.length * 25)
+    }
+
+    useEffect(() => {
+        handleValidationAndProgress()
     }, [formData])
+
+    useEffect(() => {
+        handleProgressBarCompletion()
+    }, [objectProgress])
 
     return (
         <>
@@ -31,14 +95,15 @@ export function FormWithStatusBar() {
                     <label>name</label>
                     <input
                         name="name"
-                        onChange={(ev) => { setFormData(data => ({ ...data, name: ev.target.value })) }}
+                        onChange={(ev) => { handleName(ev) }}
                     ></input>
                 </div>
                 <div className="form-input">
                     <label>value</label>
                     <select
                         name="type"
-                        onChange={(ev) => { setFormData(data => ({ ...data, type: ev.target.value })) }}
+                        onChange={(ev) => { handleType(ev) }}
+                        value={formData.type}
                     >
                         <option value="value1">Value 1</option>
                         <option value="value2">Value 2</option>
@@ -51,7 +116,7 @@ export function FormWithStatusBar() {
                     <label>state</label>
                     <select
                         name="state"
-                        onChange={(ev) => { setFormData(data => ({ ...data, state: ev.target.value })) }}
+                        onChange={(ev) => { handleState(ev) }}
                     >
                         <option value="RJ">RJ</option>
                         <option value="SP" >SP</option>
@@ -62,12 +127,10 @@ export function FormWithStatusBar() {
                     <label>city</label>
                     <input
                         htmlFor="city"
-                        onChange={(ev) => { setFormData(data => ({ ...data, city: ev.target.value })) }}
+                        onChange={(ev) => { handleCity(ev) }}
                     ></input>
                 </div>
             </div>
-
-            {JSON.stringify(formData, null, 4)}
         </>
     )
 }
