1189da21ccbe87a1df795f3968e6dee4b5fedf29
lghiggino <lghiggino@gmail.com>
2022-05-17 09:14:34 -0300

    npx tsx --init

--- React/01-github-explorer/src/index.tsx
+++ React/01-github-explorer/src/index.tsx
@@ -2,5 +2,3 @@ import { render } from 'react-dom'
 import { App } from './App';
 
 render(<App />, document.getElementById('root'))
-
-
--- React/01-github-explorer/webpack.config.js
+++ React/01-github-explorer/webpack.config.js
@@ -8,7 +8,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 module.exports = {
     mode: isDevelopment ? 'development' : 'production',
     devtool: isDevelopment ? 'eval-nosources-cheap-module-source-map' : 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -30,7 +30,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.(j|t)sx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
