00e5e3b33f147193921ca4f2a86c197112100356
lghiggino <lghiggino@gmail.com>
2022-05-17 09:44:03 -0300

    repositoryItem in TS

--- React/01-github-explorer/src/components/RepositoryList/RepositoryItem.tsx
+++ React/01-github-explorer/src/components/RepositoryList/RepositoryItem.tsx
@@ -1,10 +1,18 @@
-export function RepositoryItem({ repository }) {
+interface RepositoryItemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function RepositoryItem({ repository }: RepositoryItemProps) {
     return (
         <li>
             <strong>{repository?.name ?? 'Default'}</strong>
             <p>{repository?.description ?? 'Repo description'}</p>
-
             <a href={repository?.html_url ?? ""}>Acessar o Repositório</a>
         </li>
-    ) 
+    )
 }
+
