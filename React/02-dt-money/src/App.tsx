292ed6d4d92f80844e66a48d444325764fabb375
lghiggino <lghiggino@gmail.com>
2022-05-19 08:56:26 -0300

    remoçaõ do @emotion e adição do styled-components

--- React/02-dt-money/src/App.tsx
+++ React/02-dt-money/src/App.tsx
@@ -1,7 +1,19 @@
+import styled from "styled-components"
+
+const Title = styled.h1`
+  font-size: 64px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>
+        Hello World
+      </h1>
+
+      <Title>Teste</Title>
     </div>
   )
 }
