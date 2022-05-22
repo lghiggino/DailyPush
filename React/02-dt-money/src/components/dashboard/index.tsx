e6fb05359d539f5483ceb9b2f859234e8024aa98
lghiggino <lghiggino@gmail.com>
2022-05-21 21:04:07 -0300

    refactor com a transactionList vindo da dashboard

--- React/02-dt-money/src/components/dashboard/index.tsx
+++ React/02-dt-money/src/components/dashboard/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+import { api } from "../../services/api"
 import { Summary } from "../summary"
 import { TransactionsTable } from "../transactionsTable"
 import { Container } from "./styles"
@@ -8,11 +10,34 @@ interface DashboardProps {
     style?: any
 }
 
+export type TransactionType = {
+    id: number
+    title: string
+    amount: number
+    type: string
+    category: string
+    createdAt: string
+    userId: number
+}
+
 export function Dashboard({ props, children, style }: DashboardProps) {
+
+    const [transactionList, setTransactionList] = useState<TransactionType[]>([])
+
+    async function getTransactionData() {
+        const transactionList = await api.get('/transactions')
+        setTransactionList(transactionList.data)
+        console.log(transactionList.data)
+    }
+
+    useEffect(() => {
+        getTransactionData()
+    }, [])
+    
     return (
         <Container>
             <Summary />
-            <TransactionsTable />
+            <TransactionsTable transactionList={transactionList} />
         </Container>
     )
 }
--- React/02-dt-money/src/components/transactionsTable/index.tsx
+++ React/02-dt-money/src/components/transactionsTable/index.tsx
@@ -1,31 +1,12 @@
-
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
 import { Container } from "./styles";
+import { TransactionType } from '../dashboard'
 
 
-export type TransactionType = {
-    id: number
-    title: string
-    amount: number
-    type: string
-    category: string
-    createdAt: string
-    userId: number
+interface TransactionsTableProps {
+    transactionList: TransactionType[]
 }
 
-export function TransactionsTable() {
-    const [transactionList, setTransactionList] = useState<TransactionType[]>([])
-
-    async function getTransactionData() {
-        const transactionList = await api.get('/transactions')
-        setTransactionList(transactionList.data)
-        console.log(transactionList.data)
-    }
-
-    useEffect(() => {
-        getTransactionData()
-    }, [])
+export function TransactionsTable({ transactionList }: TransactionsTableProps) {
 
     if (transactionList.length === 0) {
         return (
@@ -50,7 +31,7 @@ export function TransactionsTable() {
                     {transactionList.map(transaction => (
                         <tr key={transaction.id}>
                             <td>{transaction.title}</td>
-                            <td className={transaction.type}>{transaction.amount}</td>
+                            <td className={transaction.type}>{transaction.type === 'withdraw' ? '-' : ''}R${transaction.amount}</td>
                             <td>{transaction.category}</td>
                             <td>{transaction.createdAt}</td>
                         </tr>
