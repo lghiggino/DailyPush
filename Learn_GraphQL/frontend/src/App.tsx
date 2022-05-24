7788562dedb1ea5154aae668223da80bd3c8fdd5
lghiggino <lghiggino@gmail.com>
2022-05-24 09:58:24 -0300

    creaUser + deleteUser with mutations

--- Learn_GraphQL/frontend/src/App.tsx
+++ Learn_GraphQL/frontend/src/App.tsx
@@ -1,43 +1,65 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { gql, useMutation, useQuery } from '@apollo/client'
+import { FormEvent, useEffect } from 'react'
 import './App.css'
+import { NewUserForm } from './components/NewUserForm'
+
+const GET_USER = gql`
+  query {
+    users {
+    id,
+    name
+    }
+  }
+`
+
+const DELETE_USER = gql`
+  mutation($id: String!) {
+  deleteUser(id: $id) {
+    id,
+    name
+  }
+}
+`
+
+type User = {
+  name: string
+  id: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, error, data } = useQuery<{ users: User[] }>(GET_USER)
+  console.log("data", data)
+  console.log("error", error)
+  const [deleteUser, { data: deleteData, error: deleteError, loading: deleteLoading }] = useMutation(DELETE_USER)
+
+  async function handleUserDeletion(id: string) {
+    console.log("deletedId",id)
+    await deleteUser({
+      variables: {
+        id
+      }
+    })
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div>
+        <NewUserForm />
+      </div>
+      <ul>
+        {data?.users.map((user): any => (
+          <li key={user.id}>
+            {user.name}
+            <button onClick={() => handleUserDeletion(user.id)}>
+              Apagar
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ Learn_GraphQL/frontend/src/components/NewUserForm.tsx
@@ -0,0 +1,45 @@
+import { gql, useMutation } from "@apollo/client"
+import { FormEvent, useState } from "react"
+
+const CREATE_USER = gql`
+    mutation ($name: String!) {
+        createUser(name: $name) {
+            id,
+            name
+        }
+    }
+`
+
+export function NewUserForm() {
+    const [name, setName] = useState('')
+    const [createUser, {data, error, loading}] = useMutation(CREATE_USER)
+
+    async function handleCreateUser(event: FormEvent) {
+        event.preventDefault()
+        if (!name){
+            return
+        }
+
+        await createUser({
+            variables: {
+                name
+            }
+        })
+    }
+
+    return (
+        <form onSubmit={handleCreateUser}>
+            <label>Name:</label>
+            <input
+                type="text"
+                value={name}
+                onChange={e => setName(e.target.value) }
+            />
+            <button type="submit">Criar Novo Usuário</button>
+        </form>
+    )
+}
+
+function UseState(arg0: string): [any, any] {
+    throw new Error("Function not implemented.")
+}
