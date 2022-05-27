78bebc9ad24ca649a01c0f4c8578122d3d8ec82e
lghiggino <lghiggino@gmail.com>
2022-05-27 08:21:43 -0300

    Book Model + schema.gql

--- Learn_GraphQL/backend/src/models/Book.ts
+++ Learn_GraphQL/backend/src/models/Book.ts
@@ -1,4 +1,15 @@
-export type Book = {
-    title: String
-    author: String
-  }
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+}) 
+
+export default mongoose.model('Book', Schema);
