96e0df6089ed96a00c021b7887bf1511cb92ab49
lghiggino <lghiggino@gmail.com>
2022-07-22 08:33:57 -0300

    setup and run with nodemon

--- BackEnd/nodeDeepDive/src/index.js
+++ BackEnd/nodeDeepDive/src/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     console.log('bateu em /')
-    res.send('banana').status(200)
+    res.send('apple').status(200)
 })
 
 const PORT = process.env.PORT || 3333
-
+console.log(`Server running on Port: ${PORT}`)
 app.listen(PORT)
