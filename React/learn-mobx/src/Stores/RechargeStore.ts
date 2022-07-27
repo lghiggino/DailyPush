076917c68400367a0fc1427cc12a09a5f184dfee
lghiggino <lghiggino@gmail.com>
2022-07-27 07:36:38 -0300

    handleConfirmRechargeValue,handleVoltzCallRequestBlockFund,handleApiCallRequestRecharge

--- React/learn-mobx/src/Stores/RechargeStore.ts
+++ React/learn-mobx/src/Stores/RechargeStore.ts
@@ -1,28 +1,35 @@
 import axios from "axios";
-import { action, makeObservable, observable, runInAction } from "mobx"
+import { action, makeObservable, observable } from "mobx"
 
 export class RechargeStore {
-    anypokemon: any = {}
     ddd: string | undefined = undefined
     phone: string | undefined = undefined
     provider: string | undefined = undefined
     partnerResponse: any[] | undefined = undefined
+    rechargeValue: string | undefined = undefined
+    rechargeFormattedValue: string = ''
+    partnerProductId: string | undefined = undefined
+    rechargeRequestId: string | undefined = undefined
 
     constructor() {
         makeObservable(this, {
-            anypokemon: observable,
             ddd: observable,
             phone: observable,
             provider: observable,
             partnerResponse: observable,
-            handleGetPokemon: action,
-            handleGetSplitPhoneNumber: action
+            rechargeValue: observable,
+            rechargeFormattedValue: observable,
+            partnerProductId: observable,
+            rechargeRequestId:observable,
+            handleSplitPhoneNumber: action,
+            handleSetProviderName: action,
         });
     }
 
-    handleGetSplitPhoneNumber = (phoneNumber: string) => {
+    // 01
+    handleSplitPhoneNumber = (phoneNumber: string) => {
         if (phoneNumber.length !== 11) {
-            return('invalid')
+            return ('invalid')
         }
         else {
             this.ddd = phoneNumber.slice(0, 2)
@@ -31,89 +38,96 @@ export class RechargeStore {
         }
     }
 
-    handleSetProviderName = (providerName: string) =>{
-        const providers = ['vivo', 'tim', 'oi', 'claro', 'nextel']
-        if (!providers.includes(providerName)){
-            alert('invalid provider name')
-            return('invalid')
+    // 02
+    handleSetProviderName = (providerName: string) => {
+        const providers = ['Vivo', 'Tim', 'Oi', 'Claro']
+        if (!providers.includes(providerName)) {
+            return ('invalid')
         }
-    }
-
-    handleApiCallLogin = async () => {
-        const username = process.env.RV_USERNAME
-        const password = process.env.RV_PASSWORD
-
-        console.log(username, password)
-        try {
-            const { data } = await axios.post('https://auth.sbx.rvhub.com.br/oauth2/token?grant_type=client_credentials',
-                {},
-                {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Authorization': 'Basic N2Y2bWFsdjBnY2NtNXJvdm1wYWp2YXBoaTY6cTJqMXZjY245MXFxcHY0MDNpamMwMmFtYW8ybmZwaXY1ZHBjZzFyaDFlbDhtYzIzYnFu'
-                    }
-                }
-            )
-
-            console.log(data)
-            const token: string = data.access_token
-            return token
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    handleApiCallGetProductByProviderNameAndAreaCode = async () => {
-        console.log('entrou aqui')
-        const token = await this.handleApiCallLogin()
-        const ddd = this.ddd
-        const provider = 'vivo'
-
-        if (!token) {
-            return 'unable to login'
-        }
-        try {
-            const { data } = await axios.post(`https://api.sbx.rvhub.com.br/portfolio/?kinds=cellphone&provider=${provider}&area_code=${ddd}`,
-                {},
-                { headers: { 'Authorization': `Bearer ${token}` } }
-            )
-            console.log(data)
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    handleGetProviderName = (providerName: string) => {
-        //precisa checar a validade do providerName se ele vem de um front controlado?
         this.provider = providerName
-        if ((this.ddd !== undefined) && (this.provider !== undefined)) {
-            this.handleGetProviderOptionsList(this.ddd, this.provider)
-        }
+        alert(this.provider)
     }
 
-    handleGetProviderOptionsList = async (ddd: string, provider: string) => {
+    // 03
+    handleApiCallGetProductByProviderNameAndAreaCode = async () => {
+        const ddd = this.ddd
+        const provider = this.provider
         try {
-            //posta pra senha
-            //get dos planos e seta this.partnerResponse
-            // const {data} = await axios.get()
+            const { data } = await axios.get(`http://localhost:3333/rv/portfolio?providerName=${provider}&areaCode=${ddd}`)
+            this.partnerResponse = data
         } catch (error) {
-            alert(error)
+            console.log(error)
         }
     }
 
-    handleGetPokemon = async (poke: string) => {
-        console.log('entrou aqui')
-        try {
-            const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${poke}`)
-            runInAction(() => {
-                return this.anypokemon = data
-            })
-        } catch (error) {
-            runInAction(() => {
-                return this.anypokemon = { undefined }
-            })
-
-        }
+    // 04
+    handleConfirmRechargeValue = (value: string, productId: string) => {
+        this.rechargeValue = value
+        this.partnerProductId = productId
+        alert(`${this.rechargeValue} ${this.partnerProductId}`)
+        this.handleVoltzCallRequestBlockFund(this.rechargeValue)
+        this.handleApiCallRequestRecharge()
     }
+
+    //05
+    handleVoltzCallRequestBlockFund = async (value: string) => {
+        const formattedValue = value.slice(0,-2)
+        this.rechargeFormattedValue = formattedValue
+        console.log('entrou aqui no handleVoltzCallRequestBlockFund')
+        //precisa chamar na voltz e validar se está tudo OK
+        await this.handleApiCallRequestRecharge()
+    }
+
+    //06
+    handleApiCallRequestRecharge = async () => {
+        const productId = this.partnerProductId
+        const areaCode = this.ddd
+        const phoneNumber = this.phone
+        const formattedValue = this.rechargeFormattedValue
+
+        const { data } = await axios.post(`http://localhost:3333/rv/recharge/request?productId=${productId}&areaCode=${areaCode}&phoneNumber=${phoneNumber}`)
+        console.log(data)
+        //chama na Voltz a transferencia do valor
+        this.handleVoltzCallRequestCreditTransfer(formattedValue)
+    }
+
+    //07
+    handleVoltzCallRequestCreditTransfer = async (value: string) => {
+        setTimeout(() => {
+            alert(`confirma a transferência de R$${value} entre user e voltz`)
+        }, 1200)
+
+        //chama na rv a confirmação da recarga
+    }
+
+    //08
+    handleApiCallConfirmRecharge = async () => {}
+
+    //09 
+    handleVoltzCallRequestBankStatement = async () => {}
+
+
+
+
+
+    // handleGetProviderName = (providerName: string) => {
+    //     //precisa checar a validade do providerName se ele vem de um front controlado?
+    //     this.provider = providerName
+    //     if ((this.ddd !== undefined) && (this.provider !== undefined)) {
+    //         this.handleGetProviderOptionsList(this.ddd, this.provider)
+    //     }
+    // }
+
+    // handleGetProviderOptionsList = async (ddd: string, provider: string) => {
+    //     try {
+    //         //posta pra senha
+    //         //get dos planos e seta this.partnerResponse
+    //         // const {data} = await axios.get()
+    //     } catch (error) {
+    //         alert(error)
+    //     }
+    // }
+
+
+
 }
