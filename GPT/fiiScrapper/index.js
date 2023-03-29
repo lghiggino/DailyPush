0d64a24f07cb9cac975adae7935799d0af0dec9b
lghiggino <lghiggino@gmail.com>
2023-03-29 08:22:19 -0300

    fix: targeted classes

--- GPT/fiiScrapper/index.js
+++ GPT/fiiScrapper/index.js
@@ -3,14 +3,13 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://br.investing.com/equities/StocksFilter?index_id=179');
+  await page.goto('https://fiis.com.br/lista-de-fundos-imobiliarios/');
 
   const boxes = await page.$$('.tickerBox');
   for (let box of boxes) {
     const title = await box.$eval('.tickerBox__title', el => el.innerText.trim());
-    const price = await box.$eval('.tickerBox__info__box > .price', el => el.innerText.trim());
-    const change = await box.$eval('.tickerBox__info__box > .change', el => el.innerText.trim());
-    console.log(title, price, change);
+    const dividendYield = await box.$eval('.tickerBox__info__box', el => el.innerText.trim());
+    console.log(title, dividendYield);
   }
 
   await browser.close();
